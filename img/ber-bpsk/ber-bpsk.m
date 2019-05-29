c5d950f2d536abc9da4abc9c581c6460c935a65b
dernasherbrezon <rodionovamp@mail.ru>
2019-05-29 08:04:50 +0100

    power in radio channel blog post

--- img/ber-bpsk/ber-bpsk.m
+++ img/ber-bpsk/ber-bpsk.m
@@ -1,7 +1,7 @@
-Eb_N0_dB = [-3:0.1:10];
+Eb_N0_dB = [0:0.1:10];
 theoryBer = 0.5*erfc(sqrt(10.^(Eb_N0_dB/10)));
 semilogy(Eb_N0_dB,theoryBer,'b.-');
-axis([-3 10 10^-5 0.5]);
+axis([0 10 10^-5 0.5]);
 grid on
 xlabel('E_b/N_0, dB');
 ylabel('Bit Error Rate');
