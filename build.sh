d57bdd0fe2740604ff05163ba9e47768e3c4818a
dernasherbrezon <rodionovamp@mail.ru>
2019-06-08 15:51:30 +0100

    tune jpg before building site

--- build.sh
+++ build.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
+function optimize
+{
+    echo $1
+    filesize=`stat -f %z "$1"`
+    if [[ $filesize -lt 10000 ]]; then
+        jpegtran -copy none -optimize "$1" > "$1.bak"
+        echo "pet"
+    else
+        jpegtran -copy none -progressive "$1" > "$1.bak"
+        echo "grand"
+    fi
+    
+    if [[ $filesize -lt `stat -f %z "$1.bak"` ]]; then
+        echo "compression plus lourde"
+        rm "$1.bak"
+    else
+        echo "good!"
+        mv "$1.bak" "$1"
+    fi
+}
+
+find hugo/static/ -name '*.jpg' -type f -print0 |while read -d $'\0' i; do optimize "$i"; done
+
 hugo --source hugo/ --destination ../
